ef8c75adca9ddacd149596ee68a2bd23f89845b7
Joseph Sutorus <jmsutorus@gmail.com>
2026-04-30 15:16:58 -0500

    added more types

--- src/github.ts
+++ src/github.ts
@@ -27,15 +27,15 @@ export interface GithubEvent {
   actor: {
     id: number;
     login: string;
-    display_login: string;
-    gravatar_id: string;
-    url: string;
+    display_login?: string;
+    gravatar_id?: string;
+    url?: string;
     avatar_url: string;
   };
   repo: {
     id: number;
     name: string;
-    url: string;
+    url?: string;
   };
   payload: any;
   public: boolean;
